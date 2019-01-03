3041423e69b284b8cfc8a439d931c545a705a0c1
EricJD <EricDussel@gmx.de>
2019-01-03 19:15:47 +0100

    tried a unified version of the logout function for Firefox, Chrome and IE

--- pkg/frontend/logout.js
+++ pkg/frontend/logout.js
@@ -1,18 +1,30 @@
 function logout() {
-    $.ajax({
-        type: "GET",
-        url: "URL",
-        dataType: 'json',
-        async: true,
-        username: "some_username_noone_has",
-        password: "any_password_not_used",
-        data: '{ "comment" }'
-    })
-        .done(function(){
-            alert('Der genutzte Browser ist zu alt oder zu speziell und unterstützt den Logout nicht. Bitte schließen sie ihren Browser oder löschen sie ihren Authentifizierungs-Cache.')
-        })
-        .fail(function(){
-            alert('Logout abgeschlossen.');
-            window.location = "/";
+    try {
+        $.ajax({
+            url: "/secure/dashboard.html",
+            username: 'please enter your username here',
+            password: 'any_password_not_used',
+
+            statusCode: {
+                401: function () {
+                    window.location = "/";
+                }
+            }
+
+                .done(function(){
+                    alert('Der genutzte Browser ist zu alt oder zu speziell und unterstützt den Logout nicht. Bitte schließen sie ihren Browser oder löschen sie ihren Authentifizierungs-Cache.')
+                })
+                .fail(function(){
+                    alert('Logout abgeschlossen.');
+                    window.location = "/";
+                })
+
         });
+    } catch (exception) {
+        if (!document.execCommand("ClearAuthenticationCache")) {
+            document.location = "https://reset:reset@" + document.location.hostname + document.location.pathname;
+        }
+    };
+
+    window.location = "/";
 }
