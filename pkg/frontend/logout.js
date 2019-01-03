571ab66ff9acb1b2660edcf7020fef80753aa539
EricJD <EricDussel@gmx.de>
2019-01-03 19:31:21 +0100

    refactored the unified logout

--- pkg/frontend/logout.js
+++ pkg/frontend/logout.js
@@ -1,5 +1,6 @@
 function logout() {
     try {
+        // for Firefox
         $.ajax({
             url: "/secure/dashboard.html",
             username: 'please enter your username here',
@@ -7,24 +8,19 @@ function logout() {
 
             statusCode: {
                 401: function () {
-                    window.location = "/";
+                    document.location = "/";
                 }
             }
 
-                .done(function(){
-                    alert('Der genutzte Browser ist zu alt oder zu speziell und unterstützt den Logout nicht. Bitte schließen sie ihren Browser oder löschen sie ihren Authentifizierungs-Cache.')
-                })
-                .fail(function(){
-                    alert('Logout abgeschlossen.');
-                    window.location = "/";
-                })
-
         });
+
     } catch (exception) {
+        // for IE
         if (!document.execCommand("ClearAuthenticationCache")) {
+            //for Chrome
             document.location = "https://reset:reset@" + document.location.hostname + document.location.pathname;
         }
-    };
+    }
 
-    window.location = "/";
+    document.location = "/";
 }
