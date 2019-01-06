3ce08cc90f2275e3a1cbcf3280d5ad01b760e44a
thehilli <theresa.hillenbrand@t-online.de>
2019-01-06 17:43:29 +0100

    empty test functions handler

--- pkg/frontend/handler_test.go
+++ pkg/frontend/handler_test.go
@@ -5,6 +5,10 @@ import (
 	"testing"
 )
 
+func TestFillTicket(t *testing.T) {
+
+}
+
 func TestOpenTickets(t *testing.T) {
 	err := os.Chdir("../../")
 	if err != nil {
@@ -14,4 +18,84 @@ func TestOpenTickets(t *testing.T) {
 	if len(tickets) == 0 {
 		t.Error("Ausgelesene Tickets sind leer")
 	}
+}
+
+func TestTicketID(t *testing.T) {
+
+}
+
+func TestSaveExternal(t *testing.T) {
+
+}
+
+func TestSave(t *testing.T) {
+
+}
+
+func TestUpdateTickets(t *testing.T) {
+
+}
+
+func TestSaveProfile(t *testing.T) {
+
+}
+
+func TestHandlerDashboard(t *testing.T) {
+
+}
+
+func TestHandlerTickets(t *testing.T) {
+
+}
+
+func TestHandlerOpenTickets(t *testing.T) {
+
+}
+
+func TestHandlerProTickets(t *testing.T) {
+
+}
+
+func TestHandlerClosedTickets(t *testing.T) {
+
+}
+
+func TestHandlerTicketDet(t *testing.T) {
+
+}
+
+func TestHandlerEntry(t *testing.T) {
+
+}
+
+func TestHandlerSave(t *testing.T) {
+
+}
+
+func TestHandlerRelease(t *testing.T) {
+
+}
+
+func TestHandlerTake(t *testing.T) {
+
+}
+
+func TestHandlerAssign(t *testing.T) {
+
+}
+
+func TestHandlerAdd(t *testing.T) {
+
+}
+
+func TestHandlerClose(t *testing.T) {
+
+}
+
+func TestHandlerProfile(t *testing.T) {
+
+}
+
+func TestHandlerSaveProfile(t *testing.T) {
+
 }
