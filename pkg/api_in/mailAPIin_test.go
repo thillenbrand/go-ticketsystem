66f202055b822985b6947a5d5754bb6698cc539f
thehilli <theresa.hillenbrand@t-online.de>
2019-01-11 19:37:40 +0100

    Tests for mailAPIin

--- pkg/api_in/mailAPIin_test.go
+++ pkg/api_in/mailAPIin_test.go
@@ -5,6 +5,8 @@ package api_in
 import (
 	"fmt"
 	"go-ticketsystem/pkg/backend"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"strconv"
 	"testing"
@@ -15,15 +17,16 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
-}
-
-func TestTicketExist(t *testing.T) {
 	backend.UpdateTickets()
 	var entry []backend.Entry
 	entry = append(entry, backend.Entry{Date: "2019-01-01", Author: "Test", Text: "Test", Visible: true})
-	ticket := &backend.Ticket{ID: len(backend.TicketsByTicketID) + 1, Subject: "Test", Status: "offen", Assigned: false, IDEditor: 0, Entry: entry}
+	ticket := &backend.Ticket{ID: len(backend.TicketsByTicketID) + 1, Subject: "Test", Status: "geschlossen", Assigned: false, IDEditor: 0, Entry: entry}
 	backend.SaveExternal(ticket)
 	backend.UpdateTickets()
+}
+
+func TestTicketExist(t *testing.T) {
+
 	mail := Mail{Address: "test@test.de", Subject: "KeinTest", Text: "Dies ist ein Test"}
 	if TicketExist(mail) != false {
 		t.Error()
@@ -35,21 +38,9 @@ func TestTicketExist(t *testing.T) {
 		fmt.Println(err)
 	}
 	backend.UpdateTickets()
-	id = strconv.Itoa(len(backend.TicketsByTicketID))
-	err = os.Remove("./pkg/tickets/ticket" + id + ".json")
-	if err != nil {
-		fmt.Println(err)
-	}
-	backend.UpdateTickets()
 }
 
 func TestTicketExist2(t *testing.T) {
-	backend.UpdateTickets()
-	var entry []backend.Entry
-	entry = append(entry, backend.Entry{Date: "2019-01-01", Author: "Test", Text: "Test", Visible: true})
-	ticket := &backend.Ticket{ID: len(backend.TicketsByTicketID) + 1, Subject: "Test", Status: "offen", Assigned: false, IDEditor: 0, Entry: entry}
-	backend.SaveExternal(ticket)
-	backend.UpdateTickets()
 	mail := Mail{Address: "test@test.de", Subject: "Test", Text: "Dies ist ein Test"}
 	if TicketExist(mail) != true {
 		t.Error()
@@ -61,4 +52,17 @@ func TestTicketExist2(t *testing.T) {
 		fmt.Println(err)
 	}
 	backend.UpdateTickets()
+}
+
+func TestHandlerCreateTicket(t *testing.T) {
+	req, err := http.NewRequest("GET", "/createTicket/~asd~asd~asd", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(HandlerCreateTicket)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusFound {
+		t.Error()
+	}
 }
